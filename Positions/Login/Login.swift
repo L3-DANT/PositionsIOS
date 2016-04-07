76217987bd7b2cb74f1273a210fb371023a5a821
bylux <m.s.luxx@gmail.com>
2016-04-07 16:48:58 +0200

    Adding User and location class

--- Positions/Login/Login.swift
+++ Positions/Login/Login.swift
@@ -9,6 +9,32 @@
 import UIKit
 
 class Login: UITableViewController {
+    
+    /* Utilisateur 
+    var utilisateur:[Utilisateur] = [Utilisateur]()
+    
+    struct Utilisateur {
+        var nom:String = ""
+        var prenom:String = ""
+        var motDePasse:String = ""
+        var pseudo:String = ""
+        //var localisation:String = ""
+        init(data:NSDictionary){
+            if let add = data["nom"] as? String {
+                self.nom = add
+            }
+            if let add = data["prenom"] as? String {
+                self.prenom = add
+            }
+            if let add = data["pseudo"] as? String {
+                self.pseudo = add
+            }
+            if let add = data["motDePasse"] as? String {
+                self.motDePasse = add
+            }
+        }
+    }*/
+    
 
     @IBOutlet weak var txtUsername: UITextField!
     
@@ -22,6 +48,8 @@ class Login: UITableViewController {
     var usernamesString:String!
     
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         passwordViewConfig()
       //  loginViewConfig()
@@ -96,14 +124,14 @@ class Login: UITableViewController {
     
     @IBAction func loginAction(sender: AnyObject) {
     
-        let url = "http://132.227.125.76:8080/Positions/utilisateur/inscription"
+        let url = "http://132.227.125.181:8080/Positions/utilisateur/inscription"
         let request = NSMutableURLRequest(URL: NSURL(string: url)!)
         
         let session = NSURLSession.sharedSession()
         request.HTTPMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
-        let params = ["nom":txtUsername.text!, "prenom":txtPassword.text!] as Dictionary<String, String>
+        let params = ["nom":txtUsername.text!, "prenom":txtPassword.text!,"pseudo":"Mp24qzasfdsqqdp", "motDePasse":"AZECODE"] as Dictionary<String, String>
         do {
             request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(params, options: [])
         } catch {
@@ -117,19 +145,68 @@ class Login: UITableViewController {
             }
             do {
                 if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
-                    let success = json["success"] as? Int
+                    let success = json["pseudo"] as? String
                     print("Success: \(success)")
+                    print((json["localisation:date"] as? String))
+                    // print(json)
+                    //self.extract_json_data(json)
+                  
+                  /*  let user = Utilisateur(nom: (json["nom"] as? String)!, prenom: (json["prenom"] as? String)!, motDePasse: (json["motDePasse"] as? String)!, pseudo: (json["pseudo"] as? String)!, latitude: (json["latitude"] as? Float)!, longitude: (json["longitude"] as? Float)!, heure: (json["heure"] as? String)!, date: (json["date"] as? String)!, token: (json["token"] as? String)!)
+                   
+                    user.affiche()
+                     */
+                    
                 } else {
                     let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
                     print("Error could not parse JSON: \(jsonStr)")
+                    if (jsonStr == true){
+                        print("Connexion reussie !");
+                    }
                 }
             } catch let parseError {
                 print(parseError)
                 let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
                 print("Error could not parse JSON: '\(jsonStr)'")
+                if (jsonStr == true){
+                    print("Connexion reussie !");
+                }
             }
         }
         task.resume()
+      
         
     }
+   
+    
+    
+    // Extraction Json Données 
+    /*
+    func extract_json_data(data:NSDictionary)
+    {
+        var json: AnyObject?
+        print("ret")
+        
+        json =  data //try NSJSONSerialization.JSONObjectWithData(data, options: [])
+        
+        
+        guard let data_array = json as? NSArray else
+        {
+            return
+        }
+        
+        
+        for(var i = 0; i < data_array.count; i++)
+        {
+            print("Affiche ")
+            utilisateur.append(Utilisateur(data: data_array[i] as! NSDictionary))
+        }
+        
+        print("Affiche un utilisateur : ")
+        print(utilisateur)
+    
+        
+    }*/
+    
+  
+    
 }
