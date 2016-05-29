bc95ba8211bf62b94b87b0498b367d2d26701a37
woolridgegrant <porteedm@gmail.com>
2016-05-29 13:54:45 +0200

    removing wrong pusher dependency

--- Pods/Target Support Files/Pods-Positions/Pods-Positions-frameworks.sh
+++ Pods/Target Support Files/Pods-Positions/Pods-Positions-frameworks.sh
@@ -82,12 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/SocketRocket/SocketRocket.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/libPusher/Pusher.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/SocketRocket/SocketRocket.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/libPusher/Pusher.framework"
-fi
