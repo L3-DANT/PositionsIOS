6cf53f23da98253d923262a5888b068a98d5e6bf
bylux <m.s.luxx@gmail.com>
2016-05-31 22:07:18 +0200

    commented line in inscription

--- Positions/Login/Inscription.swift
+++ Positions/Login/Inscription.swift
@@ -38,9 +38,9 @@ class Inscription: UIViewController {
         let MailString = Mail.text!
         let MdP = MotPasse.text!
         let VerifPasse = MotPasse1.text!
-        
-        "http://92.170.201.10/Positions/utilisateur/inscription"
-        let request = NSMutableURLRequest(URL: NSURL(string: "http://134.157.122.100:8080/Positions/utilisateur/inscription")!)
+        //http://134.157.122.100:8080/Positions/utilisateur/inscription
+        //"http://92.170.201.10/Positions/utilisateur/inscription"
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://92.170.201.10/Positions/utilisateur/inscription")!)
         if (VerifMotPass(MdP, MotPasse1String: VerifPasse)){
         EnregistrementUtilisateur.getDataAsynchronously(request, motPasse: MdP, mail: MailString, pseudo: PseudoString){data in
             print("Asynchronously fetched \(data!.length) bytes")
